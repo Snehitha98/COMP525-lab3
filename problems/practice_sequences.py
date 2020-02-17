ddb1426702328f119b83f978d1d0232d0fb16ce7
snehitha <mamidisnehitha@gmail.com>
2020-02-17 17:05:23 -0500

    Implemented months_and_days

--- problems/practice_sequences.py
+++ problems/practice_sequences.py
@@ -21,14 +21,30 @@ class Practice(object):
             each month, from January to December, and February having 28 days
             Example: [31, 28, 31, 30, 31, ...]
         Returns: list of of words, where each word has the following:
-            First three letters of a month hame, with first letter capitalized
+            First three letters of a month name, with first letter capitalized
             Followed by '-' and then the number of days in that month
 
         Example: Calling the function will produce the following list
             ['Jan-31', 'Feb-28', 'Mar-31', ... ]
         """
-        pass
+        three_letters_of_month_name=[]
+        for month in month_names:
+            three_letters_of_month_name=three_letters_of_month_name+[month[0:3]]
 
+        first_letter_capital=[]
+        for month_name in three_letters_of_month_name:
+            first_letter_capital=first_letter_capital+[month_name.capitalize()]
+
+        x=[]
+        for month_name in first_letter_capital:
+            x=x+[month_name+"-"]
+
+        expected_result=[]
+        k=range(len(month_days))
+        for i in k:
+            result=(x[i])+str(month_days[i])
+            expected_result=expected_result+[result]
+        return expected_result
 
 if __name__ == '__main__':
     p = Practice()
